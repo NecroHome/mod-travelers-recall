97809b913217cffd0700465929318c2b0ffd6429
Thiago <thiagobarretocbv@hotmail.com>
2026-05-29 18:12:19 -0300

    new locations and GM commands

--- data/sql/world/update/2026_05_29_01.sql
+++ data/sql/world/update/2026_05_29_01.sql
@@ -1,9 +1,29 @@
--- New Unique KEY so players can't unlock the same place twice
+-- Fixing The Exodar location
+UPDATE acore_world.custom_travelers_recall_locations
+SET area_id = 3557, position_x = -3965.7, position_y = -11653.6, position_z = -138.844, orientation = 0.852154
+WHERE area_id = 1357 and map_id = 530;
+
+-- New unique keys
+ALTER TABLE acore_world.custom_travelers_recall_locations
+ADD UNIQUE KEY custom_travelers_recall_locations_unique (area_id);
+
+-- Removing duplicates
+USE acore_world;
+
+DELETE t1
+FROM custom_travelers_recall_locations t1
+INNER JOIN custom_travelers_recall_locations t2
+ON t1.area_id = t2.area_id AND t1.id > t2.id;
+
+-- New unique key, area_id
+
 DROP INDEX IF EXISTS uq_guid_location
 ON acore_characters.custom_travelers_recall_unlocks;
 
 ALTER TABLE acore_characters.custom_travelers_recall_unlocks
 ADD UNIQUE KEY uq_guid_location (guid, location_id);
+
+-- New Locations
 
 -- New Locations
 INSERT IGNORE INTO acore_world.custom_travelers_recall_locations
