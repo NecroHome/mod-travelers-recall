c8954ac6bf13e48b6ccb3a7485dd36711923eadb
Thiago <thiagobarretocbv@hotmail.com>
2026-06-12 14:38:06 -0300

    SQL duplicate error

---
+++ data/sql/characters/update/TR_2026_06_12_00.sql
@@ -0,0 +1,13 @@
+SET @exists = (
+    SELECT COUNT(*)
+    FROM information_schema.statistics
+    WHERE table_schema = 'acore_characters'
+      AND table_name = 'custom_travelers_recall_unlocks'
+      AND index_name = 'uq_guid_location'
+);
+
+SET @sql = IF(
+    @exists = 0,
+    'ALTER TABLE acore_characters.custom_travelers_recall_unlocks ADD UNIQUE KEY uq_guid_location (guid, location_id)',
+    'SELECT "Index already exists"'
+);
