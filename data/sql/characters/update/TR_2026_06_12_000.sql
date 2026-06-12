ce0d5c1c40c4d8f72fab86f13902822ccc569d0b
Thiago <thiagobarretocbv@hotmail.com>
2026-06-12 14:56:36 -0300

    SQL duplicate error

--- data/sql/characters/update/TR_2026_06_12_000.sql
+++ data/sql/characters/update/TR_2026_06_12_000.sql
@@ -1,3 +1,11 @@
+CREATE TABLE IF NOT EXISTS `custom_travelers_recall_unlocks` (
+  `guid` int NOT NULL,
+  `location_id` int NOT NULL,
+  `unlocked_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `cooldown_end` int unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`guid`,`location_id`)
+);
+
 SET @exists = (
     SELECT COUNT(*)
     FROM information_schema.statistics
--- data/sql/world/update/TR_2026_05_29_001.sql
+++ data/sql/world/update/TR_2026_05_29_001.sql
@@ -11,6 +11,23 @@ FROM custom_travelers_recall_locations t1
 INNER JOIN custom_travelers_recall_locations t2
 ON t1.area_id = t2.area_id AND t1.id > t2.id;
 
+CREATE TABLE IF NOT EXISTS `custom_travelers_recall_locations` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `area_id` int NOT NULL,
+  `name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `map_id` int NOT NULL,
+  `position_x` float NOT NULL,
+  `position_y` float NOT NULL,
+  `position_z` float NOT NULL,
+  `orientation` float NOT NULL DEFAULT '0',
+  `faction` tinyint NOT NULL DEFAULT '2',
+  `icon` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `required_level` int DEFAULT '1',
+  `cooldown` int unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `idx_area_id` (`area_id`)
+);
+
 -- New unique keys
 SET @exists = (
     SELECT COUNT(*)
